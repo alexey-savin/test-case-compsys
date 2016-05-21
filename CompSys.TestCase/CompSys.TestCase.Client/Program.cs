6f23b9c8dd22c62366c018adc3ce3e6266467db4
alexey-savin <alexeyv.savin@gmail.com>
2016-05-21 11:32:31 +0300

    - race condition

--- CompSys.TestCase/CompSys.TestCase.Client/Program.cs
+++ CompSys.TestCase/CompSys.TestCase.Client/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CompSys.TestCase.Client
@@ -63,6 +64,62 @@ namespace CompSys.TestCase.Client
                 }
 
                 Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Race condition example
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <param name="serviceFactory"></param>
+        /// <param name="iterationCount"></param>
+        /// <param name="taskCount"></param>
+        private static void RunGetOrAdd(string prefix, Func<ICompSysService> serviceFactory, int iterationCount, int taskCount)
+        {
+            var barrier = new Barrier(taskCount);
+            var cts = new CancellationTokenSource();
+            var tasks = new Task<int[]>[taskCount];
+
+            for (int i = 0; i < taskCount; i++)
+            {
+                tasks[i] = Task.Run(
+                    () =>
+                    {
+                        try
+                        {
+                            var service = serviceFactory();
+                            var ids = new int[iterationCount];
+                            barrier.SignalAndWait(cts.Token);
+
+                            for (int j = 0; j < iterationCount && !cts.IsCancellationRequested; j++)
+                            {
+                                ids[j] = service.GetOrAdd(prefix + j);
+                            }
+
+                            return ids;
+                        }
+                        catch
+                        {
+                            cts.Cancel();
+                            throw;
+                        }
+                    });
+            }
+
+            var taskResults = Task.WhenAll(tasks).GetAwaiter().GetResult();
+
+            for (int i = 0; i < taskResults.Length - 1; i++)
+            {
+                var a = taskResults[i];
+                var b = taskResults[i + 1];
+
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[j] != b[j])
+                    {
+                        throw new InvalidOperationException("RunGetorAdd does not work!!!");
+                    }
+                }
             }
         }
     }
